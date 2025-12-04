80a6178bdb97cc25bad5b88869c51de1898a0c54
379005109-lab <379005109@qq.com>
2025-12-04 02:34:17 +0000

    fix: update miniapp API baseURL to ICP domain

--- miniapp/config/api.js
+++ miniapp/config/api.js
@@ -4,8 +4,8 @@
  */
 
 const config = {
-  // 后端 API 地址 - 使用小程序专用接口
-  baseURL: 'https://xiaodiyanxuan.com/api/miniapp',
+  // 后端 API 地址 - 使用ICP备案域名
+  baseURL: 'https://pkochbpmcgaa.sealoshzh.site/api/miniapp',
   
   // 请求超时时间
   timeout: 10000,
