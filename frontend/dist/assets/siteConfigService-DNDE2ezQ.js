fb1b8f6a31cd2f02bb4fe453ca9850e30a66ec0a
379005109-lab <379005109@qq.com>
2025-12-10 05:14:55 +0000

    build: 重新构建前端，隐藏材质信息

---
+++ frontend/dist/assets/siteConfigService-DNDE2ezQ.js
@@ -0,0 +1,1 @@
+import{s as a}from"./index-DWFic46w.js";const t=async()=>(await a.get("/site-config")).data.data,s=async t=>(await a.post("/site-config/batch",{configs:t})).data;export{s as b,t as g};
